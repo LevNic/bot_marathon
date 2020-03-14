838101c70d8f3ea8bb24e176dc9ebe547232e700
LevNic <klacc.f@gmail.com>
2020-03-14 10:45:28 +0300

    New group

--- config_bot.py
+++ config_bot.py
@@ -1,3 +1,5 @@
-vk_token = '507e0345fbb0c5e832d4bd2a62a78f7fb3945fdb3faf974ec5e784b2323a18bae557c164f16caf655a3cb'
-vk_grour_id = 169003564
+# vk_token = '507e0345fbb0c5e832d4bd2a62a78f7fb3945fdb3faf974ec5e784b2323a18bae557c164f16caf655a3cb'
+# vk_grour_id = 169003564
+vk_token = 'a6f9149938e073cdb5f62bc3ac0a2136a348957c7293ce0d1727891d98c4260c9712189322c82cb3fd9e4'
+vk_grour_id = 83727735
 name_marathon = 'english_marathon'
